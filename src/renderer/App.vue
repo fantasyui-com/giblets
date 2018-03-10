4ba20c6ab87fdbb4c36cb4979ed918e9b8abd298
fantasyui-com <captain@fantasyui.com>
2018-03-10 14:04:35 -0500

    Initial Build

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'giblets'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
